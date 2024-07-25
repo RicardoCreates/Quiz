03f5dd932c05004a95dcee798d3b964321f399d6
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-07-25 14:30:55 +0200

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import Navbar from "@/components/Navbar";
 import GlobalStyle from "../styles";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+      <Navbar />
+      <Footer />
     </>
   );
 }
