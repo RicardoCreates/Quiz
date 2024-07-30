011bb74d98e67fe390a85e119c1702be724f8fbc
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-07-30 10:34:16 +0200

    fixFav

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import GlobalStyle from "../styles";
 import Footer from "@/components/Footer";
 import useLocalStorageState from "use-local-storage-state";
 
+const initalFavorite = Array(10).fill(false);
+
 export default function App({ Component, pageProps }) {
   const [favorites, setFavorites] = useLocalStorageState("favorites", {
     defaultValue: initalFavorite,
